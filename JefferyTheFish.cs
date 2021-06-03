c3c9cf75c2aca5d9835040e44a72fd744cf1bc89
MichaelNichxls <70662676+MichaelNichxls@users.noreply.github.com>
2021-06-03 16:30:15 -0500

    Initial commit

---
+++ JefferyTheFish.cs
@@ -0,0 +1,8 @@
+using Terraria.ModLoader;
+
+namespace JefferyTheFish
+{
+	public class JefferyTheFish : Mod
+	{
+	}
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# JefferyTheFish
+ A mod for tModLoader that adds a fish pet named Jeffery.
